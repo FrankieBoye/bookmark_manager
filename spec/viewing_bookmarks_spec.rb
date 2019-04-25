a562dd5d2fc82657e4dcd750241c5daf942ffe37
FrankieBoye <frankie4@btinternet.com>
2019-04-25 16:40:49 +0100

    code working correctly and fixed rubocop errors

--- spec/viewing_bookmarks_spec.rb
+++ spec/viewing_bookmarks_spec.rb
@@ -8,9 +8,9 @@ end
 feature 'Viewing bookmarks' do
   scenario 'A user can see bookmarks' do
 
-  Bookmark.create(url: 'http://www.makersacademy.com')
-  Bookmark.create(url: 'http://www.destroyallsoftware.com')
-  Bookmark.create(url: 'http://www.google.com')
+    Bookmark.create(url: 'http://www.makersacademy.com')
+    Bookmark.create(url: 'http://www.destroyallsoftware.com')
+    Bookmark.create(url: 'http://www.google.com')
 
     visit('/bookmarks')
 
